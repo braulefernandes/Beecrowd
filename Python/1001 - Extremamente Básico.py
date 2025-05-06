92c75d413e60b00541de26789a42a180a443565e
BrauleFernandes <braulefernandes@yahoo.com>
2025-05-06 12:02:45 -0300

    Questão BeeCrowd

---
+++ Python/1001 - Extremamente Básico.py
@@ -0,0 +1,3 @@
+primeiro_valor = int(input())
+segundo_valor = int(input())
+print(f"X = {primeiro_valor + segundo_valor}")
---
+++ Python/1003 - Soma Simples .py
@@ -0,0 +1,5 @@
+primeiro_valor = int(input())
+segundo_valor = int(input())
+soma = primeiro_valor + segundo_valor
+
+print(f"SOMA = {soma}")
