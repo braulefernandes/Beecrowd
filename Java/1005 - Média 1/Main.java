024ebfece4a4203610715198c4a0aac004647072
BrauleFernandes <braulefernandes@yahoo.com>
2025-07-09 15:52:42 -0300

    Questão Beecrowd

---
+++ Java/1005 - Média 1/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        double A = input.nextDouble();
+        double B = input.nextDouble();
+
+        double media = (A * 3.5 + B * 7.5) / 11;
+
+        System.out.printf("MEDIA = %.5f%n", media);
+
+        input.close();
+    }
+}
