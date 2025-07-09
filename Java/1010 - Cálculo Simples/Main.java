b76e04750e45ed069b70491825e089ca6e5998c1
BrauleFernandes <braulefernandes@yahoo.com>
2025-07-09 15:56:09 -0300

    Questão Beecrowd

---
+++ Java/1010 - Cálculo Simples/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int codigo1 = input.nextInt();
+        int quantidade1 = input.nextInt();
+        double valor1 = input.nextDouble();
+
+        int codigo2 = input.nextInt();
+        int quantidade2 = input.nextInt();
+        double valor2 = input.nextDouble();
+
+        double total = quantidade1 * valor1 + quantidade2 * valor2;
+
+        System.out.printf("VALOR A PAGAR: R$ %.2f%n", total);
+
+        input.close();
+    }
+}
