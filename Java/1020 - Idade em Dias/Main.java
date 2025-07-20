4fe4b2f47623190cd9fc669f2465bf2936063b90
BrauleFernandes <braulefernandes@yahoo.com>
2025-07-20 19:36:54 -0300

    Questão Beecrowd

---
+++ Java/1020 - Idade em Dias/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int dias = input.nextInt();
+
+        int anos = dias / 365;
+        dias %= 365;
+
+        int meses = dias / 30;
+        dias %= 30;
+
+        System.out.printf("%d ano(s)%n", anos);
+        System.out.printf("%d mes(es)%n", meses);
+        System.out.printf("%d dia(s)%n", dias);
+
+        input.close();
+    }
+}
