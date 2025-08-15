ba0687c0cdc49f303a73f287e17e95ce24f5a1ca
BrauleFernandes <braulefernandes@yahoo.com>
2025-08-15 02:31:04 -0300

    Questão Beecrowd

---
+++ JavaScript/1037 - Intervalo.js
@@ -0,0 +1,13 @@
+var n = parseFloat(require('fs').readFileSync('/dev/stdin', 'utf8'));
+
+if (n < 0 || n > 100) {
+    console.log("Fora de intervalo");
+} else if (n <= 25) {
+    console.log("Intervalo [0,25]");
+} else if (n <= 50) {
+    console.log("Intervalo (25,50]");
+} else if (n <= 75) {
+    console.log("Intervalo (50,75]");
+} else {
+    console.log("Intervalo (75,100]");
+}
