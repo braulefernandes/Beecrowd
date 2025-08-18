5f34e68710debb0cfba5b01b0bd1c8dcde67c202
BrauleFernandes <braulefernandes@yahoo.com>
2025-08-18 07:35:31 -0300

    Questão Beecrowd

---
+++ JavaScript/1050 - DDD.js
@@ -0,0 +1,7 @@
+var ddd = parseInt(require('fs').readFileSync('/dev/stdin', 'utf8'));
+var mapa = {
+    61: "Brasilia", 71: "Salvador", 11: "Sao Paulo", 21: "Rio de Janeiro",
+    32: "Juiz de Fora", 19: "Campinas", 27: "Vitoria", 31: "Belo Horizonte"
+};
+
+console.log(mapa[ddd] || "DDD nao cadastrado");
