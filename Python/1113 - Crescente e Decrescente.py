c9ddb89b77b5bb5b17b91175a1b0f25d44ab4066
BrauleFernandes <braulefernandes@yahoo.com>
2025-09-04 14:59:21 -0300

    Questão Beecrowd

---
+++ Python/1113 - Crescente e Decrescente.py
@@ -0,0 +1,8 @@
+while(True):
+    x,y = map(int,input().split(" "))
+    if(x!=y and x>y):
+        print("Decrescente")
+    elif(x!=y and x<y):
+        print("Crescente")
+    else:
+        break
